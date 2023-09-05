d7f8e0e3154c2aff79adf4db4909e687e55e8035
MatyBene <benedettimaty@gmail.com>
2023-09-05 16:05:43 -0300

    agrega test de autosParaLaVenta

--- test/services/main-services.test.js
+++ test/services/main-services.test.js
@@ -68,4 +68,87 @@ describe("mainServices", () => {
     //Assert(que quiero esperar, y lo compara con lo que llego)
     expect(result).toEqual(mockAutos[0]);
   });
+  /* it("Si le damos una patente deberia devolver que el auto esta vendido", () => {
+    //Arrange
+    const mockAutos = [
+      {
+        patente: "APL123",
+        vendido: false,
+      },
+    ];
+
+    concesionaria.autos = mockAutos;
+    //Act
+    const result = concesionaria.venderAuto("APL123");
+    //Assert
+    expect(result).toEqual(mockAutos[0].vendido);
+  }); */
+  it("Deberia devolver la lista de autos que estan disponibles para la venta", () => {
+    //Arrange
+    const mockAutos = [
+      {
+        marca: "Ford",
+        modelo: "Fiesta",
+        precio: 150000,
+        km: 200,
+        color: "Azul",
+        cuotas: 12,
+        anio: 2019,
+        patente: "APL123",
+        vendido: false,
+      },
+      {
+        marca: "Toyota",
+        modelo: "Corolla",
+        precio: 100000,
+        km: 0,
+        color: "Blanco",
+        cuotas: 14,
+        anio: 2019,
+        patente: "JJK116",
+        vendido: false,
+      },
+      {
+        marca: "Toyota",
+        modelo: "Corolla",
+        precio: 100000,
+        km: 0,
+        color: "Blanco",
+        cuotas: 14,
+        anio: 2019,
+        patente: "JJK116",
+        vendido: true,
+      },
+    ];
+    concesionaria.autos = mockAutos;
+
+    const mockDisponibles = [
+      {
+        marca: "Ford",
+        modelo: "Fiesta",
+        precio: 150000,
+        km: 200,
+        color: "Azul",
+        cuotas: 12,
+        anio: 2019,
+        patente: "APL123",
+        vendido: false,
+      },
+      {
+        marca: "Toyota",
+        modelo: "Corolla",
+        precio: 100000,
+        km: 0,
+        color: "Blanco",
+        cuotas: 14,
+        anio: 2019,
+        patente: "JJK116",
+        vendido: false,
+      },
+    ];
+    //Act
+    const result = concesionaria.autosParaLaVenta();
+    //Assert
+    expect(result).toEqual(mockDisponibles);
+  });
 });
