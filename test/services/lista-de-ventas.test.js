0bcabdd7a1d994c747fbd236f2fd8c84d7237fb2
MatyBene <benedettimaty@gmail.com>
2024-06-30 16:16:59 -0300

    agrega test, deberia devolver un array vacio si no hay ningun auto vendido, a listaDeVentas

--- test/services/lista-de-ventas.test.js
+++ test/services/lista-de-ventas.test.js
@@ -16,4 +16,22 @@ describe("listaDeVentas", () => {
       mockAutos[9].precio,
     ]);
   });
+
+  test("deberia devolver un array vacio si no hay ningun auto vendido", () => {
+    concesionaria.autos = [
+      {
+        marca: "Ford",
+        modelo: "Fiesta",
+        precio: 150000,
+        km: 100,
+        color: "Azul",
+        cuotas: 12,
+        anio: 2019,
+        patente: "APL123",
+        vendido: false,
+      },
+    ];
+    const result = concesionaria.listaDeVentas();
+    expect(result).toEqual([]);
+  });
 });
