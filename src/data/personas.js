e981a54d3002849876562f6975fdeffb2e2dd19c
MatyBene <benedettimaty@gmail.com>
2024-07-02 12:01:38 -0300

    agrega archivo personas.js

---
+++ src/data/personas.js
@@ -0,0 +1,34 @@
+let personas = [
+  {
+    nombre: "Juan",
+    capacidadDePagoEnCuotas: 20000,
+    capacidadDePagoTotal: 100000,
+  },
+  {
+    nombre: "Ana",
+    capacidadDePagoEnCuotas: 15000,
+    capacidadDePagoTotal: 80000,
+  },
+  {
+    nombre: "Luis",
+    capacidadDePagoEnCuotas: 25000,
+    capacidadDePagoTotal: 120000,
+  },
+  {
+    nombre: "Sofía",
+    capacidadDePagoEnCuotas: 18000,
+    capacidadDePagoTotal: 90000,
+  },
+  {
+    nombre: "Carlos",
+    capacidadDePagoEnCuotas: 22000,
+    capacidadDePagoTotal: 110000,
+  },
+  {
+    nombre: "María",
+    capacidadDePagoEnCuotas: 20000,
+    capacidadDePagoTotal: 100000,
+  },
+];
+
+module.exports = personas;
