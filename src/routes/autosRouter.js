80814f05f9ec3a5fb3638a6400c047485e82d7c6
MatyBene <benedettimaty@gmail.com>
2024-07-04 01:13:08 -0300

    comienza ruteo. crea carpeta views

--- src/routes/autosRouter.js
+++ src/routes/autosRouter.js
@@ -1,5 +1,9 @@
 const { Router } = require("express");
 const router = Router();
 
+const autosController = require("../controllers/autosController");
 
+// router.get('/', autosController.listaAutos);
+
+// router.get('/:patente', autosController.detalleAuto);
 
