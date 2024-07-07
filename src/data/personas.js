aceb277adc2671e8aef682eb2b17536de9cb4d06
MatyBene <benedettimaty@gmail.com>
2024-07-07 16:49:59 -0300

    agrega id a persona

--- src/data/personas.js
+++ src/data/personas.js
@@ -1,30 +1,36 @@
 let personas = [
   {
+    id: 0,
     nombre: "Juan",
     capacidadDePagoEnCuotas: 20000,
     capacidadDePagoTotal: 100000,
   },
   {
+    id: 1,
     nombre: "Ana",
     capacidadDePagoEnCuotas: 15000,
     capacidadDePagoTotal: 80000,
   },
   {
+    id: 2,
     nombre: "Luis",
     capacidadDePagoEnCuotas: 25000,
     capacidadDePagoTotal: 120000,
   },
   {
+    id: 3,
     nombre: "Sofía",
     capacidadDePagoEnCuotas: 18000,
     capacidadDePagoTotal: 90000,
   },
   {
+    id: 4,
     nombre: "Carlos",
     capacidadDePagoEnCuotas: 22000,
     capacidadDePagoTotal: 110000,
   },
   {
+    id: 5,
     nombre: "María",
     capacidadDePagoEnCuotas: 20000,
     capacidadDePagoTotal: 100000,
